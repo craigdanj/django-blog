a60dc584497f3e72fd80532bdd297ee6dd8b1978
Craig <craigdanj@gmail.com>
2018-07-26 12:44:58 +0530

    first migration

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+	name = models.CharField(max_length=80)
+	short_description = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.name
+
+class Post(models.Model):
+	post_title = models.CharField(max_length=200)
+	post_description = models.CharField(max_length=5000)
+	pub_date = models.DateTimeField('date published')
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.post_title
