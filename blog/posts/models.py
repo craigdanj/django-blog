d8bdd4c806d781fa105868c44c9e569aa91866a3
Craig <craigdanj@gmail.com>
2018-07-26 15:58:35 +0530

    table for tags added

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -9,11 +9,15 @@ class Category(models.Model):
 	def __str__(self):
 		return self.name
 
+class Tag(models.Model):
+	name = models.CharField(max_length=100)
+
 class Post(models.Model):
 	#description = models.CharField(max_length=5000)
 	#pub_date = models.DateTimeField('date published')
 	title = models.CharField(max_length=200)
 	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+	tag = models.ManyToManyField(Tag)
 
 	def __str__(self):
 		return self.post_title
