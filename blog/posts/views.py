92a5ace6934be406e9878edb721fc335ef22d203
Craig <craigdanj@gmail.com>
2018-07-27 14:02:09 +0530

    template and view for single post updated

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -14,8 +14,17 @@ def posts(request):
 	return render(request, 'posts/posts.html', context)
 
 def post(request, post_id):
+
+	post = Post.objects.get(pk=post_id)
+	categories = Category.objects.all()
+	tags = Tag.objects.all()
+
 	context = {
 		'id': post_id,
+		'post': post,
+		'category_list': categories,
+		'tag_list': tags,
 	}
+
 	return render(request, 'posts/post.html', context)
 
