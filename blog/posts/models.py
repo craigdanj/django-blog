2a2ef195107c7a4d3195fa7c8633e9093579f8f0
Craig <craigdanj@gmail.com>
2018-07-27 12:44:51 +0530

    posts models tweaked

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 class Category(models.Model):
 	name = models.CharField(max_length=80)
-	description = models.CharField(max_length=100)
 
 	def __str__(self):
 		return self.name
@@ -12,12 +11,15 @@ class Category(models.Model):
 class Tag(models.Model):
 	name = models.CharField(max_length=100)
 
+	def __str__(self):
+		return self.name
+
 class Post(models.Model):
-	#description = models.CharField(max_length=5000)
-	#pub_date = models.DateTimeField('date published')
+	description = models.TextField(default="")
+	pub_date = models.DateTimeField('date published', default=None)
 	title = models.CharField(max_length=200)
-	category = models.ForeignKey(Category, on_delete=models.CASCADE)
-	tag = models.ManyToManyField(Tag)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
+	tag = models.ManyToManyField(Tag, blank=True)
 
 	def __str__(self):
-		return self.post_title
+		return self.title
