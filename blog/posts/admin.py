eea9bba2c0a969931cf3b1cce65f13902d25ef50
Craig <craigdanj@gmail.com>
2018-07-27 11:18:31 +0530

    tag added to admin

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Category, Post, Tag
 
 # Register your models here.
-admin.site.register(Category)
 admin.site.register(Post)
+admin.site.register(Category)
 admin.site.register(Tag)
