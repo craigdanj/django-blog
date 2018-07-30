3f526cbddcdc92352828f71f00f8d6607ba1d564
Craig <craigdanj@gmail.com>
2018-07-30 14:11:01 +0530

    admin view for posts includes flitering via taxonomies now

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Category, Post, Tag
 
 # Register your models here.
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title', 'category', 'pub_date')
+	list_filter = ['category', 'tag']
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
