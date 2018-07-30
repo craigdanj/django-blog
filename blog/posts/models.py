9d1967ce0f88dc8c09b6df9f90774b900dea119f
Craig <craigdanj@gmail.com>
2018-07-30 13:57:31 +0530

    plural name for categories added

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -8,6 +8,9 @@ class Category(models.Model):
 	def __str__(self):
 		return self.name
 
+	class Meta:
+		verbose_name_plural = "categories"
+
 class Tag(models.Model):
 	name = models.CharField(max_length=100)
 
